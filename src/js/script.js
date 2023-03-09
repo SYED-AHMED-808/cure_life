e7888c584b2a431b188267645a7333155fa5933a
SYED AHMED <107113388+SYED-AHMED-808@users.noreply.github.com>
2023-03-10 01:14:31 +0530

    created modal floting needed more css focus

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 console.log("welcome to js file");
+// nav bar configurations starts
 let cross = document.getElementById("cross");
 // console.log(cross)
 let home = document.getElementById("ham-bar");
@@ -18,4 +19,31 @@ cross.addEventListener('click', () => {
         box.classList.toggle('moveout');
     }, 100)
 })
-console.log(home)
+// nav bar configuration ends
+// register and sign up starts
+let modal1 = document.getElementById("register");
+modal1.addEventListener('mouseover', (e) => {
+    document.getElementById('registerbox').style.display = 'block';
+})
+modal1.addEventListener('mouseleave', (e) => {
+    setTimeout(() => {
+
+        document.getElementById('registerbox').style.display = 'none';
+    }, 1000);
+})
+//modal 2
+let modal2 = document.getElementById("signup");
+modal2.addEventListener('mouseover', (e) => {
+    document.getElementById('signupbox').style.display = 'block';
+})
+modal2.addEventListener('mouseleave', (e) => {
+    setTimeout(() => {
+
+        document.getElementById('signupbox').style.display = 'none';
+    }, 1000);
+})
+
+
+
+
+// console.log(floter)  
