22d419f5a1ee2dbbacc11218035c7b89d6e7aef9
SYED AHMED <107113388+SYED-AHMED-808@users.noreply.github.com>
2023-03-09 22:46:37 +0530

    add nav its transition and changed cursors to pointer

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,21 @@
 console.log("welcome to js file");
 let cross = document.getElementById("cross");
-console.log(cross)
-cross.addEventListener('click', () => {
-    console.log("ima ")
+// console.log(cross)
+let home = document.getElementById("ham-bar");
+home.addEventListener('click', () => {
+    let box = document.getElementById("ham-menu-box");
+    // console.log(box);
+    setTimeout(() => {
+
+        box.classList.toggle('moveout');
+    }, 300)
 })
+
+cross.addEventListener('click', () => {
+    let box = document.getElementById("ham-menu-box");
+    setTimeout(() => {
+
+        box.classList.toggle('moveout');
+    }, 100)
+})
+console.log(home)
